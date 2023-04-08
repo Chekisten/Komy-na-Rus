dc34b2a42cdb29071dbac9a6f764e12b7abdb889
Chekisten <sgtbelov@gmail.com>
2023-04-08 11:15:01 +0300

    добавелные файлы php для шапки и подвала

---
+++ components/header.php
@@ -0,0 +1,15 @@
+
+<header>
+    <ul>
+        <li>
+            <a href="#"> Карта </a>
+        </li>
+        <li>
+            <a href="#"> Блог </a>
+        </li>
+        <li>
+            <a href="#"> О проекте </a>
+        </li>
+    </ul>
+</header>
+

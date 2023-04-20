037d1bffb3137fb3a0c733123affd8f04ff55351
Chekisten <sgtbelov@gmail.com>
2023-04-20 10:43:13 +0300

    страница блог, about, стили

---
+++ blog.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="ru">
+    <head>
+        <meta charset="utf-8">
+        <title>Блог "Кому на Руси жить хорошо"</title>
+        <link rel="stylesheet" href="styles/main.css">
+    </head>
+    <body>
+        <?php require("components/header.php");
+        ?>
+
+        <main>
+            <section class="main-blog">
+                <h1>Блог Кому на Руси жить хорошо</h1>
+                <p>В данном разделе будут публиковаться статьи по социологии и экологии</p>
+            </section>
+            <section class="blog-article">
+                <article>
+                    <h2>Первая статья</h2>
+                    <img src="img/glush_250_200.png">
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusantium, ipsam necessitatibus deserunt id facilis veniam dolor nisi esse at sint velit debitis provident odio non iste minus ratione, similique tempora.</p>
+                </article>
+
+            </section>
+
+        </main>
+
+        <?php require("components/footer.php");
+        ?>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ru">
+    <head>
+        <meta charset="utf-8">
+        <title>Кому на Руси жить хорошо</title>
+        <link rel="stylesheet" href="styles/main.css">
+    </head>
+    <body>
+        <?php require("components/header.php");
+        ?>
+
+        <main>
+            <section>
+                <h1> Кому на Руси жить хорошо <h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, magnam voluptates! Eum fugit, omnis at aspernatur modi, distinctio quo id incidunt possimus officia aliquid cumque. Doloribus fuga magnam repellendus exercitationem?</p>
+            </section>
+        </main>
+
+        <?php require("components/footer.php");
+        ?>
+    </body>
+
+</html> 
